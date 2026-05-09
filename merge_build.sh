e9637702279bc263da67d7431614fd5380072c2e
uni-lamb <3240100672@zju.edu.cn>
2026-05-09 11:14:01 +0800

    fix: 锁定补丁版本为 3.2.0

--- merge_build.sh
+++ merge_build.sh
@@ -68,7 +68,7 @@ DOWNLOAD_APKEEP() {
     local FILENAME="apkeep"
 
     echo "正在下载apkeep工具..."
-    local API_RESPONSE=$(curl -s "https://api.github.com/repos/${OWNER}/${REPO}/releases/tags/3.2.0")
+    local API_RESPONSE=$(curl -s "https://api.github.com/repos/${OWNER}/${REPO}/releases/latest")
     local DOWNLOAD_LINK=$(echo "${API_RESPONSE}" | jq -r ".assets[] | select(.name | contains(\"${LIB_PLATFORM}\")) | .browser_download_url" | head -n 1)
     if [ -z "${DOWNLOAD_LINK}" ] || [ "${DOWNLOAD_LINK}" == "null" ]; then
         echo "无法找到Apkeep下载链接"
